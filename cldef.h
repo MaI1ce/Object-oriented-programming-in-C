1cd1573e22f2f624d519c399c6c855a8af92efe3
MaI1ce <vladyslavshapoval21@gmail.com>
2022-01-20 20:07:17 +0100

    Add macroses CTOR and DTOR and bug fix

--- cldef.h
+++ cldef.h
@@ -1,20 +1,44 @@
 #ifndef CLDEF_H
 #define CLDEF_H
 
-#define CLASS_DEFINE(NAME) typedef struct NAME {        \
+#define CLASS_DECLARATION(NAME) typedef struct NAME {   \
     const void * class;                                 \
 
-#define END_CLASS_DEFINE(NAME)                          \
+
+#define END_CLASS_DECLARATION(NAME)                     \
 } NAME;                                                 \
 extern const void * _##NAME##_ ;                        \
+                                                        \
+void wrapper_##NAME##_ctor(NAME * _self, ...);          \
+                                                        \
+void wrapper_##NAME##_dtor(NAME * _self);               \
 
+
+#define CTOR(NAME, self, ...)                           \
+wrapper_##NAME##_ctor(&self __VA_OPT__(,) __VA_ARGS__)
+
+#define DTOR(NAME, self)                                \
+wrapper_##NAME##_dtor(&self)
 
 
 #define CONSTRUCTOR(NAME)                               \
                                                         \
 static void * NAME##_ctor(void * _self, va_list * arg); \
                                                         \
+void wrapper_##NAME##_ctor(NAME * _self, ...)           \
+{                                                       \
+    va_list varlist;                                    \
+    va_start(varlist, _self);                           \
+    NAME##_ctor(_self, &varlist);                       \
+    va_end(varlist);                                    \
+}                                                       \
+                                                        \
 static void * NAME##_dtor(void * _self);                \
+                                                        \
+void wrapper_##NAME##_dtor(NAME * _self)                \
+{                                                       \
+    NAME##_dtor(_self);                                 \
+}                                                       \
                                                         \
 static const _Class_ _##NAME = {                        \
     sizeof(NAME),                                       \
--- main2.c
+++ main2.c
@@ -8,13 +8,20 @@ int main(void)
 {
     INT * ob = new(INT);
 
-    printf("ob->val = %d\n", ob->val);
+    INT ob2;
+
+    CTOR(INT,ob2);
+
 
     ob->val = 10;
-
     printf("ob->val = %d\n", ob->val);
 
+    ob2.val = 11;
+    printf("ob2->val = %d\n", ob2.val);
+
     delete(ob);
+
+    DTOR(INT,ob2);
 
     return 0;
 }
